e58dad409b05dc93e58b55c7f0022087f506a84a
6Jonathan6 <jpr1993mm@hotmail.com>
2018-06-13 17:33:58 -0500

    Creacion de la user pool y creando un usuario en la base de datos de manera local

---
+++ createUser.js
@@ -0,0 +1,50 @@
+const {Pool} = require('pg')
+const dbData = require('./data.js')
+
+const event = {
+    nombre:'juan',
+    apellido:'perez',
+    sexo:'m',
+    edad:'mm',
+    telefono:'5530202464',
+    prospera:'TRUE',
+    email:'ejemplo@ejemplo.com'
+}
+
+//User pool
+
+const pool = new Pool({
+    user: dbData.user,
+    host: dbData.host,
+    database: dbData.db,
+    password: dbData.password,
+    port:dbData.port
+})
+
+pool.on('error',(err,client)=>{
+    console.log('Unexpected error on idle client',err)
+    process.exit(-1)
+})
+pool.on('connect',()=>{
+    console.log('connected')
+})
+const query = 'INSERT INTO pacientes VALUES(DEFAULT,$1,$2,$3,$4,$5,$6,$7) returning id'
+let handler= async (event) =>{
+    const parameters = []
+    for(parameter in event){
+        parameters.push(event[parameter])
+    }
+    
+    const client = await pool.connect()
+    try {
+        const res = await client.query(query,parameters).catch(e=>{throw(e)})
+        console.log(res.rows)
+    }catch(err){
+        console.log(err)
+    }  finally{
+        client.release()
+    }
+
+}
+
+    handler(event)
---
+++ data.js
@@ -0,0 +1,13 @@
+const user = 'postgresCourse'
+const db= 'Consultorio'
+const password = 'postgresCourse'
+const host = 'postgrescourse.cmsrjf692emw.us-east-1.rds.amazonaws.com'
+const port = 5432
+module.exports = {
+    user,
+    db,
+    host,
+    password,
+    host,
+    port
+}
