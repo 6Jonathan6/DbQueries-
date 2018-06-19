5fdab00f770ee31cdee0e2999e6f5393edcd59c8
6Jonathan6 <jpr1993mm@hotmail.com>
2018-06-19 00:27:21 -0500

    todas las queries para escribir algo en la db

--- createUser.js
+++ createUser.js
@@ -2,13 +2,29 @@ const {Pool} = require('pg')
 const dbData = require('./data.js')
 
 const event = {
-    nombre:'juan',
+    nombre:'Maria',
     apellido:'perez',
-    sexo:'m',
-    edad:55,
+    sexo:'f',
+    edad:26,
     telefono:'5530202464',
     prospera:'TRUE',
-    email:'ejemplo@ejemplo.com'
+    email:'ejemplo@ejemplo.com',
+    method:'regPaciente'
+}
+
+const event2 = {
+    idPaciente: 66687,
+    fechaUMen: '2018-06-18',
+    method:  'regEmbarazada'
+}
+
+const event3 = {
+    // [pacientId,fecha,hora]
+    idPaciente:66687,
+    fecha:'2018-06-22',
+    hora:'12:30',
+    method: 'regCita'
+
 }
 
 //User pool
@@ -18,40 +34,92 @@ const pool = new Pool({
     host: dbData.host,
     database: dbData.db,
     password: dbData.password,
-    port:dbData.port
+    port:dbData.port,
+    max:1,
+    idleTimeoutMillis:1
 })
 
 pool.on('error',(err,client)=>{
-    console.log('Unexpected error on idle client',err)
-    process.exit(-1)
+    //console.log('Unexpected error on idle client',err)
+    throw(err)
 })
 pool.on('connect',()=>{
     console.log('connected')
 })
-const query = 'INSERT INTO pacientes VALUES(DEFAULT,$1,$2,$3,$4,$5,$6,$7) returning id'
+pool.on('acquire',()=>{
+    console.log('new client')
+    console.log('IdleCLients'+ pool.idleCount)
+    console.log('TotalClienst' + pool.totalCount)
+    console.log('Waiting' + pool.waitingCount)})
+
+pool.on('remove',()=>{console.log('clientRemoved')})
+
+
+function registrarpaciente(client,parameters){
+    const queryPaciente = 'INSERT INTO pacientes VALUES(DEFAULT,$1,$2,$3,$4,$5,$6,$7) RETURNING id'
+    return client.query(queryPaciente,parameters)
+}
+
+function registrarEmbarazada(client,parameters){
+    const queryEmbarazada = 'INSERT INTO embarazadas VALUES(DEFAULT,$1,$2) RETURNING id '
+    return client.query(queryEmbarazada,parameters)
+}
+
+function registrarCita(client,parameters){
+    const queryCita = 'INSERT INTO citas (id,fecha,hora,id_paciente) VALUES(DEFAULT,$1,$2,$3) RETURNING *'
+    return client.query(queryCita,parameters)
+}
+
 let handler= async (event) =>{
-    const parameters = []
-    for(parameter in event){
-        parameters.push(event[parameter])
-    }
-    
     const client = await pool.connect()
     const dbResponse ={}
+  
     try {
-        const res = await client.query(query,parameters).catch(e=>{throw(e)})
-        console.log(res.rows[0])
-        dbResponse.id = res.rows[0].id
-
+        switch(event.method){
+            case 'regPaciente':
+                    parameters=[
+                    
+                    event.nombre,
+                    event.apellido,
+                    event.sexo,
+                    event.edad,
+                    event.telefono,
+                    event.prospera,
+                    event.email,
+                ]
+                response = await registrarpaciente(client,parameters).catch(e=>{throw(e)})
+                dbResponse.id = response.rows[0].id
+                break
+            
+            case 'regEmbarazada':
+                parameters = [
+                    event.fechaUMen,
+                    event.idPaciente
+                ]
+                response = await registrarEmbarazada(client,parameters).catch(e=>{throw(e)})
+                dbResponse.id = response.rows[0].id
+                break
+            case 'regCita':
+                parameters = [
+                    event.fecha,
+                    event.hora,
+                    event.idPaciente
+                ]
+                response = await registrarCita(client,parameters).catch(e=>{throw(e)})
+                dbResponse.cita = response.rows[0]
+                break
+                
+        }
     }catch(err){
         console.log(err)
-        dbResponse.error = err
+        dbResponse.error = err.code
+        console.log(dbResponse.error)
 
     }  finally{
         client.release()
-        console.log('final')
+        console.log('Succed, client release')
         return dbResponse
     }
-
 }
 /*
     Las async functions siempre retornan un promesa
@@ -59,4 +127,6 @@ let handler= async (event) =>{
     cuando las invocas cons el sdk de AWS 
  */
 
-handler(event).then(dbResponse=> console.log(dbResponse))
+handler(event).then(dbResponse => console.log(dbResponse))
+handler(event2).then(dbResponse => console.log(dbResponse))
+handler(event3).then(dbResponse => console.log(dbResponse))
