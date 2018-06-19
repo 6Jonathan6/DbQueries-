ea7a9d056f59ab094fb24dd8437c447d6d0d29bc
6Jonathan6 <jpr1993mm@hotmail.com>
2018-06-19 15:16:09 -0500

    Api to find and register patients, schedule appoinments

--- createUser.js
+++ createUser.js
@@ -1,31 +1,59 @@
 const {Pool} = require('pg')
 const dbData = require('./data.js')
 
-const event = {
-    nombre:'maria',
-    apellido:'perez',
-    sexo:'f',
-    edad:26,
-    telefono:'5530202464',
-    prospera:'TRUE',
-    email:'ejemplo@ejemplo.com',
-    method:'regPaciente'
+function registrarpaciente(client,parameters){
+    const queryPaciente = 'INSERT INTO pacientes VALUES(DEFAULT,$1,$2,$3,$4,$5,$6,$7) RETURNING id'
+    return client.query(queryPaciente,parameters)
 }
 
-const event2 = {
-    idPaciente: 66717,
-    fechaUMen: '2018-06-18',
-    method:  'regEmbarazada'
+function registrarEmbarazada(client,parameters){
+    const queryEmbarazada = 'INSERT INTO embarazadas VALUES(DEFAULT,$1,$2) RETURNING id '
+    return client.query(queryEmbarazada,parameters)
 }
 
-const event3 = {
-    // [pacientId,fecha,hora]
-    idPaciente:66717,
-    fecha:'2018-06-22',
-    hora:'12:30',
-    method: 'regCita'
-
+function registrarCita(client,parameters){
+    const queryCita = 'INSERT INTO citas (id,fecha,hora,id_paciente) VALUES(DEFAULT,$1,$2,$3) RETURNING *'
+    return client.query(queryCita,parameters)
 }
+
+function getUserId(client,parameters,method){
+    /* Para hacer que postgres haga queries  sin tomar encuenta los acento en los nombre y apellidos
+        tenemos que crear la extension unaccent con el siguiente query.
+
+            CREATE EXTENSION unaccent;
+
+        EN cada database que se quiera esto es necesario crear la extension
+
+        initcap(column) juan => Juan
+    */
+    const queryEmail = 'SELECT id, initcap(nombre) AS nombre, initcap(apellido) AS apellido, email  FROM pacientes WHERE email=$1'
+    const queryName = "SELECT id, initcap(nombre) AS nombre,initcap(apellido) AS apellido FROM pacientes WHERE unaccent(nombre) LIKE $1;"
+    const queryNameSurname = "SELECT id, initcap(nombre) AS nombre,initcap(apellido) AS apellido FROM pacientes WHERE unaccent(nombre) LIKE $1 AND unaccent(apellido) LIKE $2;"
+    const queryByid = "SELECT id, initcap(nombre) AS nombre, initcap(apellido) AS apellido FROM pacientes WHERE id= $1;"
+
+    switch(method){
+        case 'getUsersByEmail':
+            return client.query(queryEmail,parameters)
+
+        case 'getUsersByName':
+            return client.query(queryName,parameters)
+
+        case 'getUsersByName':
+            return client.query(queryName,parameters)
+
+        case 'getUsersByNameSurname':
+            return client.query(queryNameSurname,parameters)
+
+        case 'getUserById':
+            return client.query(queryByid,parameters)
+    }
+}
+
+function getUserInfo(client,parameters){
+    const getInfoQuery = 'SELECT * FROM pacientes  WHERE id = $1;'
+    return client.query(getInfoQuery,parameter)
+}
+
 
 //User pool
 
@@ -52,36 +80,22 @@ pool.on('acquire',()=>{
     console.log('TotalClienst' + pool.totalCount)
     console.log('Waiting' + pool.waitingCount)})
 
-pool.on('remove',()=>{console.log('clientRemoved')})
+    pool.on('remove',()=>{console.log('clientRemoved')})
 
 
-function registrarpaciente(client,parameters){
-    const queryPaciente = 'INSERT INTO pacientes VALUES(DEFAULT,$1,$2,$3,$4,$5,$6,$7) RETURNING id'
-    return client.query(queryPaciente,parameters)
-}
 
-function registrarEmbarazada(client,parameters){
-    const queryEmbarazada = 'INSERT INTO embarazadas VALUES(DEFAULT,$1,$2) RETURNING id '
-    return client.query(queryEmbarazada,parameters)
-}
+    let handler= async (event) =>{
+        const client = await pool.connect()
+        const dbResponse ={}
 
-function registrarCita(client,parameters){
-    const queryCita = 'INSERT INTO citas (id,fecha,hora,id_paciente) VALUES(DEFAULT,$1,$2,$3) RETURNING *'
-    return client.query(queryCita,parameters)
-}
-
-let handler= async (event) =>{
-    const client = await pool.connect()
-    const dbResponse ={}
-  
     try {
         switch(event.method){
             case 'regPaciente':
             parameters=[
-                
-                event.nombre,
-                event.apellido,
-                event.sexo,
+
+                event.nombre.toLowerCase(),
+                event.apellido.toLowerCase(),
+                event.sexo.toLowerCase(),
                 event.edad,
                 event.telefono,
                 event.prospera,
@@ -90,20 +104,17 @@ let handler= async (event) =>{
             response = await registrarpaciente(client,parameters).catch(e=>{throw(e)})
             dbResponse.id = response.rows[0].id
             break
-            
+
             case 'regEmbarazada':
-                if(event.sexo != 'f'){
-                    throw(new Error('Men cannot get pregnant!'))
-                }
             parameters = [
-                    event.fechaUMen,
-                    event.idPaciente
-                ]
-                response = await registrarEmbarazada(client,parameters).catch(e=>{throw(e)})
-                dbResponse.id = response.rows[0].id
-                break
+                event.fechaUMen,
+                event.idPaciente
+            ]
+            response = await registrarEmbarazada(client,parameters).catch(e=>{throw(e)})
+            dbResponse.id = response.rows[0].id
+            break
             case 'regCita':
-                parameters = [
+            parameters = [
                     event.fecha,
                     event.hora,
                     event.idPaciente
@@ -111,27 +122,123 @@ let handler= async (event) =>{
                 response = await registrarCita(client,parameters).catch(e=>{throw(e)})
                 dbResponse.cita = response.rows[0]
                 break
-            case 'getUsersByEmail':
-                break
-                
-        }
-    }catch(err){
-        console.log(err)
-        dbResponse.error = err.code ? err.code : err  
-        console.log(dbResponse.error)
 
-    }  finally{
-        client.release()
-        console.log('Succed, client release')
-        return dbResponse
+                case 'getUserIdByEmail':
+                parameters = [
+                    event.email
+                ]
+                response = await getUserId(client,parameters,'getUsersByEmail' )
+                dbResponse.users = response.rows
+                break
+
+                case 'getUserIdByName':
+                parameters= [
+                    event.nombre.toLowerCase() + '%'
+                ]
+                response = await getUserId(client,parameters,'getUsersByName')
+                dbResponse.users = response.rows
+                break
+
+                case 'getUserIdByNameSurName':
+                parameters = [
+                    event.nombre.toLowerCase() + '%' ,
+                    event.apellido.toLowerCase() + '%'
+                ]
+                response = await getUserId(client,parameters,'getUsersByNameSurName')
+                dbResponse.users = response.rows
+                break
+
+                case 'getUserIdById':
+                parameters = [
+                    event.id
+                ]
+                response = await getUserId(client,parameters,'getUserById')
+                dbResponse.users = response.rows
+                break
+
+                case 'getUserInfo':
+                parameters = [
+                    event.id
+                ]
+                response = await getUserInfo(clien,parameters)
+                dbResponse = response.rows[0]
+                break
+
+            }
+
+        }catch(err){
+            console.log(err)
+            dbResponse.error = err.code ? err.code : err
+            console.log(dbResponse.error)
+
+        }  finally{
+            client.release()
+            console.log('Succed, client release')
+            return dbResponse
+        }
     }
-}
-/*
+    /*
     Las async functions siempre retornan un promesa
     aunque en el caso de lambda functions regresan un json
-    cuando las invocas cons el sdk de AWS 
- */
+    cuando las invocas cons el sdk de AWS
+    */
 
-handler(event).then(dbResponse => console.log(dbResponse))
-handler(event2).then(dbResponse => console.log(dbResponse))
-handler(event3).then(dbResponse => console.log(dbResponse))
+const event = {
+       nombre:'marías',
+       apellido:'perez',
+       sexo:'f',
+       edad:26,
+       telefono:'5530202464',
+       prospera:'TRUE',
+       email:'ejemplo5@ejemplo.com',
+       method:'regPaciente'
+}
+
+const event2 = {
+       idPaciente: 66717,
+       fechaUMen: '2018-06-18',
+       method:  'regEmbarazada'
+}
+
+const event3 = {
+       // [pacientId,fecha,hora]
+       idPaciente:66717,
+       fecha:'2018-06-22',
+       hora:'12:30',
+       method: 'regCita'
+
+}
+
+const event4 = {
+       email: "ejemplo@ejemplo.com",
+       method:"getUserIdByEmail"
+}
+
+const event5 = {
+    method :'getUserIdByName',
+    nombre :'Maria',
+    apellido:'Sanchez'
+}
+
+const event6 = {
+    method:'getUserIdById',
+    id: 66719
+}
+
+
+//handler(event).then(dbResponse => console.log(dbResponse))
+//handler(event2).then(dbResponse => console.log(dbResponse))
+//handler(event3).then(dbResponse => console.log(dbResponse))
+//handler(event4).then(dbResponse => console.log(dbResponse.users))
+//handler(event5).then(dbResponse => console.log(dbResponse.users))
+handler(event6).then(dbResponse => console.log(dbResponse.users))
+
+
+   /*
+   Para conbertir juan => Juan
+   function capitalize(string){
+
+       return string[0].toUpperCase() + string.substr(1)
+    }
+
+    */
